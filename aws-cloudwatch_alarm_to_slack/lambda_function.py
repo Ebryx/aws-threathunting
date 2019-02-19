dd68d5617a5c380efee6d412c7a3be14fe938b6f
Rana Awais <rana.awais@ebryx.com>
2019-02-20 00:43:07 +0500

    added functionality to fetch metric widgets from AWS using passed alarm event, post widget images to s3 and then add the urls to slack push message as attachments. added logging for script as well.

--- aws-cloudwatch_alarm_to_slack/lambda_function.py
+++ aws-cloudwatch_alarm_to_slack/lambda_function.py
@@ -1,30 +1,36 @@
+import os
 import json
+import time
+import logging
 
 import boto3
 import requests
+from elasticsearch import Elasticsearch
 
 
-##### Global Vars #######
-OPTIONS = {
-    'slack_webhook': 'https://hooks.slack.com/services/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'
-}
-#########################
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
+logger.addHandler(handler)
 
 
 def parsed_msg(msg_dict, appender=str()):
     slack_msg_parsed = str()
     for key, val in msg_dict.items():
         try:
-            
+
             # there was an issue where value against message key was being
             # interpreted as a string instead of dictionary
             # therefore, forcing the value to convert to dictionary
             if key == 'Message' and type(val) == str:
                 val = json.loads(val)
-            
+
             if type(val) is not list and type(val) is not dict:
                 # me and my appender
-                slack_msg_parsed += '`{0}_{1}`: *{2}*\n'.format(appender, key, val)
+                slack_msg_parsed += '`{0}_{1}`: *{2}*\n'.format(
+                    appender, key, val)
             elif type(val) == list:
                 # for each item
                 for idx, _ in enumerate(val):
@@ -42,77 +48,204 @@ def parsed_msg(msg_dict, appender=str()):
                 # appender and me
                 slack_msg_parsed += parsed_msg(
                     val, appender='{0}_{1}'.format(appender, key))
-            else: print('Passing key: "{0}", value: "{1}" pair')
+            else:
+                logger.info('Passing key: `{0}`, value: '
+                            '`{1}` pair'.format(key, val))
 
         except Exception as e:
-            print('Exception "{0}" occurred in for loop in parsed_msg, '
-                  'when parsing \nkey "{1}" and \nvalue {2}'.format(e, key, val))
+            logger.info('Exception {0} occurred in for loop in parsed_msg, '
+                        'when parsing key `{1}` and value `{2}`'.format(
+                            e, key, val))
 
     return slack_msg_parsed
 
 
-def report_to_slack(msg):
-
-    global OPTIONS
+def report_to_slack(msg, images):
 
     try:
-        print('Preparing slack message request')
         m_parsed_msg = parsed_msg(msg)
-        print('Message has been parsed successfully')
+        logger.info('Message has been parsed successfully.')
         title_part = str()
 
-        try: 
+        try:
             title_part = msg['Records'][0]['Sns']['Subject']
-        except Exception as e: 
-            print('Exception "{0}" occurred while setting title_part'.format(e))
+        except Exception as e:
+            logger.error(e)
 
-        slack_message_to_post = {'attachments': [
-            {
-                'color': '#ff0000',
-                'title': title_part,
-                'text': m_parsed_msg,
-                'fallback': title_part
-            }
-        ]}
+        attachments = [{
+            'color': '#ff0000',
+            'title': 'SAMPLE via python\n' + title_part,
+            'text': m_parsed_msg,
+            'fallback': title_part
+        }]
+        attachments.extend([{
+            'color': '#ff0000',
+            'footer': img['title'],
+            'author_name': 'Graph Image',
+            'author_link': img['url'],
+            'image_url': img['url']
+        } for img in images])
 
-        print('Slack message post has been prepared: %s' % (slack_message_to_post))
+        slack_message_to_post = {'attachments': attachments}
+
+        logger.debug('Slack message post \n: %s' % (slack_message_to_post))
         result = requests.post(
-            OPTIONS['slack_webhook'],
+            os.environ.get('SLACK_WEBHOOK'),
             data=json.dumps(slack_message_to_post),
             headers={'Content-Type': 'application/json'})
 
-        print('Alamr Name print: %s' % (title_part))
-        print('Posted to slack "{0}"'.format(result.text))
+        logger.info(str())
+        logger.info('Alarm Name: %s', title_part)
+        logger.info('Posted to slack: %s', result.text)
 
     except Exception as e:
-        print('Exception "{0}" occurred in report_to_slack ' \
-              'when reporting msg \n"{1}"'.format(e, msg))
+        logger.error(e)
 
 
 def get_cloudwatch_graph(event):
 
-    metric = {
-        'width': 600,
-        'height': 400,
-        'metrics': [
-            ['AWS/RDS', 'NetworkTransmitThroughput',
-             'DBInstanceIdentifier', 'o3-development']
-        ]
-    }
+    try:
+        trigger = event['Records'][0]['Sns']['Message']['Trigger']
+    except KeyError:
+        trigger = dict()
 
-    boto
+    if not trigger:
+        logger.info('No trigger found in event.')
+        return
+
+    metric = [trigger['Namespace'], trigger['MetricName']]
+    try:
+        alarm = event['Records'][0]['Sns']['Message']['AlarmName']
+    except KeyError:
+        alarm = None
+
+    target_dims = {'DBInstanceIdentifier': str()}
+    if trigger['Dimensions']:
+        for dim in trigger['Dimensions']:
+            if dim['name'] in target_dims:
+                target_dims[dim['name']] = dim['value']
+
+            metric.extend([dim['name'], dim['value']])
+
+    id_string = alarm + '/' if alarm else str()
+    id_string += '/'.join(target_dims.values())
+
+    images = list()
+    cloudwatch = boto3.client('cloudwatch')
+    variations = [
+        {
+            'start': '-PT3H',
+            'title': 'Last 3 Hours (%s)' % (id_string),
+            'period': 60 * 5,
+            'width': 800,
+            'height': 200
+        },
+        {
+            'start': '-PT24H',
+            'title': 'Last 24 Hours (%s)' % (id_string),
+            'period': 60 * 10,
+            'width': 1000,
+            'height': 230
+        },
+        {
+            'start': '-PT168H',
+            'title': 'Last 7 Days (%s)' % (id_string),
+            'period': 60 * 20,
+            'width': 1600,
+            'height': 320
+        }
+    ]
+
+    for option in variations:
+        widget = {
+            'width': option['width'],
+            'height': option['height'],
+            'start': option['start'],
+            'metrics': [metric],
+            'title': option['title'],
+            'period': option['period']
+        }
+
+        res = cloudwatch.get_metric_widget_image(
+            MetricWidget=json.dumps(widget))
+
+        images.append({
+            'title': option['title'],
+            'data': res.get('MetricWidgetImage')
+        })
+
+    return images
+
 
 def lambda_handler(event, context):
 
-    print('Event passed:\n%s' % (json.dumps(event, indent=2)))
-    get_cloudwatch_graph(event)
-    report_to_slack(event)
+    images = list()
+    es_url = os.environ.get('ELASTICSEARCH_URL')
+    es_index = os.environ.get('ELASTICSEARCH_INDEX')
+    if not (es_url and es_index):
+        logger.info('ELASTICSEARCH_URL environment variable not found. '
+                    'Skipping dump to elasticsearch.')
+    else:
+        try:
+            es = Elasticsearch(es_url)
+            es.index(index=es_index, doc_type='alarm', body=event)
+            logger.info('Forwarded event to elasticsearch: '
+                        '%s@%s', es_url, es_index)
+        except Exception as exc:
+            logger.info(exc)
+
+    s3path = os.environ.get('S3_PATH')
+    if not s3path:
+        logger.info('S3_PATH environment variable not found. '
+                    'Skipping fetch of graph images.')
+
+    elif '.com/' not in s3path:
+        logger.info('S3_PATH should be a path to an online S3 bucket. '
+                    'Skipping fetch of graph images.')
+
+    else:
+
+        logger.info('Fetching graph images from cloudwatch...')
+        images = get_cloudwatch_graph(event)
+
+        logger.info('Posting images to s3...')
+        for image in images:
+            if not image:
+                continue
+
+            im_name = 'image_%s.png' % (str(time.time()).replace('.', ''))
+            im = open(im_name, 'wb')
+            im.write(image['data'])
+            im.close()
+
+            s3 = boto3.client(
+                's3',
+                aws_access_key_id=os.environ.get('ACCESS_KEY_ID'),
+                aws_secret_access_key=os.environ.get('SECRET_ACCESS_KEY'))
+
+            bucket = s3path.split('.com/')[1].split('/')[0].strip('/')
+            path = s3path.split(bucket)[-1]
+            path = os.path.join(path, im_name).lstrip('/')
+
+            s3.upload_file(im_name, bucket, path,
+                           ExtraArgs={'ACL': 'public-read'})
+
+            image['url'] = os.path.join(s3path, im_name) 
+            os.remove(im_name)
+
+    if not os.environ.get('SLACK_WEBHOOK'):
+        logger.info('SLACK_WEBHOOK environment variable not found. '
+                    'Skipping push to slack.')
+    else:    
+        report_to_slack(event, images)
 
     return {
         "statusCode": 200,
-        "body": json.dumps({'message': 'GTS Lambda executed successfully!'})
+        "body": json.dumps({
+            'message': 'GTS Lambda executed successfully!'})
     }
 
+
 if __name__ == "__main__":
-    event = json.load(open('sample.json', 'r'))
-    lambda_handler(event, dict())
+
+    lambda_handler(dict(), dict())
